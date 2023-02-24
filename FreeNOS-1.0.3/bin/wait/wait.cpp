6de5cf14c5a7e2901e7aa3a1251f928e0af5e8ce
Angel Zuniga <angel.zuniga1067@gmail.com>
2023-02-23 23:50:10 -0800

    Added wait implementation

--- FreeNOS-1.0.3/bin/wait/wait.cpp
+++ FreeNOS-1.0.3/bin/wait/wait.cpp
@@ -5,11 +5,13 @@
 #include <ProcessClient.h>
 #include "wait.h"
 
+// Used ProcessList and 
+
 Wait::Wait(int argc, char **argv) 
 	: POSIXApplication(argc,argv)
 {
-	parser().setDescription();
-	parser().registerPositional;
+	parser().setDescription("Interrupts the process until a change in state.");
+	parser().registerPositional("PROCESS_ID", "Command that interrupts until the arguement changes the state.");
 }
 
 Wait::~Wait()
@@ -18,5 +20,34 @@ Wait::~Wait()
 
 Wait::Result Wait::exec()
 {
+	// Creating process object
+	const ProcessClient process;
+
+	// Creating process ID
+	const ProcessID pid;
+
+	// Getting the processID
+	pid = atio(arguements().get("PROCESS_ID"));
+
+	// Creating info object;
+	const ProcessClient::Info info;
+
+	// Creating result object;
+	const ProcessClient:: Result result;
+
+	// Getting info from the process using pid, and info
+	result = process.processInfo(pid, info);
+
+	// Getting the process id that is being requested
+	const ProcessID current_process = arguements().get("PROCESS_ID");
+
+	if(result != ProcessClient::Success)
+	{
+		// Using error implementation from MakeNode
+		ERROR("No ID detected in the process: " << current_process << "is not found");
+		return IOError;
+	}
+	elsewaitpid(pid, 0, 0);
+
 	return Success;
 }
