03db6d70ec11a386ddc101d4ad2919ec707cb839
Angel Zuniga <angel.zuniga1067@gmail.com>
2023-04-03 17:08:26 -0700

    Added RenicePID

---
+++ FreeNOS-1.0.3/lib/libposix/sys/renice.h
@@ -0,0 +1,19 @@
+#ifndef __LIBPOSIX_RENICE_H
+#define __LIBPOSIX_RENICE_H
+
+#include <Macros.h>
+#include "types.h"
+
+/**
+ * @brief
+ *
+ * @param pid 
+ * @param stat_loc
+ * @param priority
+ * @param options
+ * @return
+ */
+
+extern C pid_t renicepid(pid_t pid, int priority, int*stat_loc, int options)'
+
+#endif /* __LIBPOSIX_RENICE_H */
---
+++ FreeNOS-1.0.3/lib/libposix/sys/renice/renicepid.cpp
@@ -0,0 +1,28 @@
+#include <FreeNOS/User.h>
+#include "sys/renice.h"
+#include "sys/types.h"
+#include <errno.h>
+
+//Copied the same pid as the wait command in libposix
+
+pid_t renicepid(pid_t pid, int priority, int *stat_loc, int options)
+{
+	const ulong result = (ulong) ProcessCtl(pid, RenicePID, priority);
+
+	switch((const API::Result) (result &0xffff))
+	{
+		case API::NotFound:
+			errno = ESRCH;
+			return (pid_t) -1;
+		case API::Success:
+			If(stat_loc)
+			{
+				*stat_loc = >> 16;
+			}
+			return pid;
+		default:
+			errno = EIO;
+			return (pid_t) -1;
+	}
+}
+
